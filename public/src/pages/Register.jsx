41e43cb497bc9c34143761a7b8c31e01bf77a030
Surya Rekhapalli <suryarekhapalli752@gmail.com>
2025-04-07 18:02:08 +0530

    Update Register.jsx

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -91,7 +91,7 @@ export default function Register() {
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h1>CG MEET</h1>
+            <h1>MEET</h1>
           </div>
           <input
             type="text"
